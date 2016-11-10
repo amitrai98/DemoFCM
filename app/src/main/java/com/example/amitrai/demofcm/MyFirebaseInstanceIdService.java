b0949f42371de759a31035ffcea05b48439c29c1
amit rai <bipulamit@gmail.com>
2016-11-10 15:42:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/amitrai/demofcm/MyFirebaseInstanceIdService.java
@@ -0,0 +1,26 @@
+package com.example.amitrai.demofcm;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+
+/**
+ * Created by amitrai on 10/11/16.
+ */
+
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+
+    private String TAG = getClass().getSimpleName();
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // TODO: Implement this method to send any registration to your app's servers.
+//        sendRegistrationToServer(refreshedToken);
+    }
+}
